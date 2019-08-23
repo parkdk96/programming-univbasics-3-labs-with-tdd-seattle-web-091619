2a8de53702c65a90a88e21334a52035265661489
Daniel Park <parkdk96@gmail.com>
2019-08-23 03:52:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,9 @@ first_number = 5.0
 second_number = 3.4
 
 sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
